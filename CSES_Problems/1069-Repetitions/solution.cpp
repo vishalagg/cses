3b2d1b45338af1bdc5ea6ff734beaa44a5c17829
Vishal Aggarwal <vishalagg1994@gmail.com>
2026-02-13 20:44:40 +0530

    Add CSES problem 1069: Repetitions solution

---
+++ CSES_Problems/1069-Repetitions/solution.cpp
@@ -0,0 +1,28 @@
+// CSES Problem 1069: Repetitions
+// Link: https://cses.fi/problemset/task/1069/
+// Description: You are given a DNA sequence: a string consisting of characters A, C, G, and T. ...
+
+#include <bits/stdc++.h>
+
+#define endl '\n'
+using namespace std;
+using ll = long long;
+const int MAX = 2 * 100100;
+long double PI = acos(-1.0);
+int mod = 1e9 + 7;
+
+signed main() {
+    string s;
+    cin >> s;
+
+    char prev = '\0';
+    int count = 0, mx = 0;
+
+    for (char c: s) {
+        if (prev == c) count++;
+        else count = 1;
+        prev = c;
+        mx = max(mx, count);
+    }
+    cout << mx;
+}
