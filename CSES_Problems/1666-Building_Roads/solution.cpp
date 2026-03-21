7239ced840cab0caf623e7109505b6a7dd8da25c
Vishal Aggarwal <vishalagg1994@gmail.com>
2026-03-21 14:50:33 +0530

    Add CSES problem 1666: Building Roads solution

---
+++ CSES_Problems/1666-Building_Roads/solution.cpp
@@ -0,0 +1,8 @@
+// CSES Problem 1666: Building Roads
+// Link: https://cses.fi/problemset/task/1666/
+// Description: Byteland has n cities, and m roads between them. The goal is to construct new ro...
+
+input/code.cpp: In function 'int main()':
+input/code.cpp:50:24: warning: comparison of integer expressions of different signedness: 'int' and 'std::vector<int>::size_type' {aka 'long unsigned int'} [-Wsign-compare]
+50 |         for (int i=1; i<mp.size(); i++) {
+    |                       ~^~~~~~~~~~
