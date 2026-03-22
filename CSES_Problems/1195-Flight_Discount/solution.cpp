b02cc70f2fec7f795bbbc9420a2bc8c8e517478f
Vishal Aggarwal <vishalagg1994@gmail.com>
2026-03-22 16:05:01 +0530

    Add CSES problem 1195: Flight Discount solution

--- CSES_Problems/1195-Flight_Discount/solution.cpp
+++ CSES_Problems/1195-Flight_Discount/solution.cpp
@@ -4,68 +4,50 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+
 #define ll long long
 
 int n, m;
 vector<vector<pair<int, ll>>> graph;
-
-vector<vector<ll>> dist;
-using state=tuple<ll, int, int>; // <dist, node, cupounUsed>
-
-void dijkastra() {
-    dist.resize(n + 1, vector<ll>(2, 1e18));
-    dist[1][0] = 0;
-
-    priority_queue<state> q;
-    q.push({-0, 1, 0});
-    vector<vector<int>> vis;
-    vis.resize(n + 1, vector<int>(2, 0));
-
-    while (!q.empty()) {
-
-        auto [cdis, cnode, cused] = q.top();
-        q.pop();
-        cdis = -cdis;
-
-        if (vis[cnode][cused])  continue;
-        vis[cnode][cused] = 1;
-
-
-        for (pair<int, ll> nbr: graph[cnode]) {
-            int v = nbr.first;
-            ll w = nbr.second;
-
-            if (dist[v][cused] > cdis + w) {
-                dist[v][cused] = cdis + w;
-                q.push({-dist[v][cused], v, cused});
-            }
-
-            if (cused == 0) {
-                ll new_w = w / 2;
-                if (dist[v][1] > cdis + new_w) {
-                    dist[v][1] = cdis + new_w;
-                    q.push({-dist[v][1], v, 1});
-                }
-            }
-        }
-    }
-}
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0); cout.tie(0);
 
     cin >> n >> m;
-    graph.resize(n + 1);
+    graph.resize(2 * n + 1);
+
     for (int i=0; i<m; i++) {
         int a, b, c;
         cin >> a >> b >> c;
 
         graph[a].push_back({b, c});
+        graph[n + a].push_back({n + b, c});
+        graph[a].push_back({n + b, c / 2});
     }
 
-    dijkastra();
+    vector<ll> dist(2 * n + 1, 1e18);
+    priority_queue<pair<ll, int>> q;
+    dist[1] = 0;
+    q.push({-0, 1});
 
-    cout << min(dist[n][0], dist[n][1]) << "\n";
+    vector<int> vis(2*n + 1, 0);
 
+    while (!q.empty()) {
+        auto [d, node] = q.top();
+        q.pop();
+        d = -d;
+
+        if (vis[node]) continue;
+        vis[node] = 1;
+
+        for (auto [v, w]: graph[node]) {
+            if (dist[v] > d + w) {
+                dist[v] = d + w;
+                q.push({-dist[v], v});
+            }
+        }
+    }
+
+    cout << min(dist[n], dist[2*n]) << "\n";
 }
