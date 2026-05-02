d6efee4af564573ab43f382f8d633dffbb228076
Vishal Aggarwal <vishalagg1994@gmail.com>
2026-05-02 13:07:18 +0530

    Add CSES problem 1647: Static Range Minimum Queries solution

---
+++ CSES_Problems/1647-Static_Range_Minimum_Queries/solution.cpp
@@ -0,0 +1,53 @@
+// CSES Problem 1647: Static Range Minimum Queries
+// Link: https://cses.fi/problemset/task/1647/
+// Description: Given an array of n integers, your task is to process q queries of the form: wha...
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[200100];
+int tree[800400];
+
+void build(int i, int l, int r) {
+    if (l == r) {
+        tree[i] = arr[l];
+        return;
+    }
+    int mid = l + (r - l)/2;
+    build(2 * i, l, mid);
+    build(2 * i + 1, mid + 1, r);
+    tree[i] = min(tree[2*i], tree[2*i+1]);
+}
+
+int query(int i, int l, int r, int lq, int rq) {
+    if (lq > r || l > rq)
+    return 1e9;
+
+    if (lq <= l && r <= rq)
+    return tree[i];
+
+    int mid = l + (r - l)/2;
+
+    return min(query(2*i, l, mid, lq, rq), query(2*i+1, mid+1, r, lq, rq));
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, q;
+    cin >> n >> q;
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    build(1, 0, n-1);
+
+    while (q--) {
+        int x, y;
+        cin >> x >> y;
+        x--; y--;
+        cout << query(1, 0, n-1, x, y) << "\n";
+    }
+
+}
