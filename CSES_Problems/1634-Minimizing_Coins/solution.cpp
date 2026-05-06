a1ee7bb69c25987db890117b9bee002a0b247895
Vishal Aggarwal <vishalagg1994@gmail.com>
2026-05-06 20:40:22 +0530

    Add CSES problem 1634: Minimizing Coins solution

---
+++ CSES_Problems/1634-Minimizing_Coins/solution.cpp
@@ -0,0 +1,36 @@
+// CSES Problem 1634: Minimizing Coins
+// Link: https://cses.fi/problemset/task/1634/
+// Description: Consider a money system consisting of n coins. Each coin has a positive integer ...
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 1e9 + 7;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n, x;
+    cin >> n >> x;
+    vector<int> arr(n);
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+
+    vector<int> dp(x + 1, 1e9);
+
+    dp[0] = 0;
+
+    for (int i=1; i<=x; i++) {
+        for (int coin: arr) {
+            if (i - coin >= 0)
+            dp[i] = min(dp[i], dp[i - coin] + 1);
+        }
+    }
+
+    if (dp[x] == 1e9)
+    cout << "-1";
+    else
+    cout << dp[x];
+}
