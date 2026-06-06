3803fa673b86adc74a405989eb0ae67490c9c9f1
Vishal Aggarwal <vishalagg1994@gmail.com>
2026-06-06 21:24:44 +0530

    Add CSES problem 1717: Christmas Party solution

---
+++ CSES_Problems/1717-Christmas_Party/solution.cpp
@@ -0,0 +1,28 @@
+// CSES Problem 1717: Christmas Party
+// Link: https://cses.fi/problemset/task/1717/
+// Description: There are n children at a Christmas party, and each of them has brought a gift. ...
+
+#include <bits/stdc++.h>
+using namespace std;
+
+const int mod = 1e9 + 7;
+
+int main() {
+    // your code goes here
+    ios::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    int n;
+    cin >> n;
+
+    vector<long long> dp(n + 1, 0);
+
+    //dp[i] = (i-1)[dp[i-2] + dp[i-1]];
+    dp[0] = 1; dp[1] = 0;
+
+    for (int i=2; i<=n; i++) {
+        dp[i] = ((i-1) * ((dp[i-2] + dp[i-1]) % mod)) % mod;
+    }
+
+    cout << dp[n];
+}
